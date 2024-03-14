e9f17bf12767f6906988bb4d90670771fe443742
Junaid <junaidofficialnow@gmail.com>
2024-03-14 23:24:04 +0530

    [social-network] domain: entities updated

--- libs/common/typings/src/lib/interfaces/IRelationship.interface.ts
+++ libs/common/typings/src/lib/interfaces/IRelationship.interface.ts
@@ -1,6 +1,18 @@
-export interface IRelationship {
-  id: string;
-  followerId: string;
-  followeeId: string;
-  createdAt: number;
+export namespace SocialGraphEntities {
+  export interface IBlockRelationship {
+    userId: string;
+    blockedUserId: string;
+    createdAt: number;
+  }
+
+  export interface IUser {
+    userId: string;
+    username: string;
+  }
+
+  export interface IFollowRelationship {
+    followerId: string;
+    followeeId: string;
+    createdAt: number;
+  }
 }
