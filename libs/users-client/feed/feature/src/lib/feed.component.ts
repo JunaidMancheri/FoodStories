fe4cb6b1ef1453ff2ccc5eac0211d9962d590366
Junaid <junaidofficialnow@gmail.com>
2024-04-05 07:45:49 +0530

    refactor: ui  cleanup

--- libs/users-client/feed/feature/src/lib/feed.component.ts
+++ libs/users-client/feed/feature/src/lib/feed.component.ts
@@ -1,8 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, inject } from '@angular/core';
+import { IUser } from '@food-stories/common/typings';
+import { selectCurrentUser } from '@food-stories/users-client/shared/app-init';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'fs-feed',
   templateUrl: 'feed.component.html',
   styleUrls: ['feed.component.css'],
 })
-export class FeedComponent {}
+export class FeedComponent implements OnInit {
+  user!: IUser;
+  store = inject(Store);
+  ngOnInit(): void {
+    this.store
+      .select(selectCurrentUser)
+      .subscribe((user) => (this.user = user));
+  }
+}
