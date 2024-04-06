f7b2fdf6e3033d613bfeb3d54bb26e576a1e0614
Junaid <junaidofficialnow@gmail.com>
2024-04-06 10:45:16 +0530

    fix: loadAppConfig not throwing error bug fixed
    ;

--- libs/common/utils/src/lib/server-utils/loadAppConfig.util.ts
+++ libs/common/utils/src/lib/server-utils/loadAppConfig.util.ts
@@ -5,20 +5,23 @@ type StringIndexedObject = {
   [key: string]: string
 }
 
-export async function loadAppConfig(
+export function loadAppConfig(
   envKeys: (keyof StringIndexedObject)[],
   appConfig: StringIndexedObject,
   logger: ILogger
 ) {
   dotenv.config();
+  let hasError = false;
 
   for (const key of envKeys) {
     const value = process.env[key];
     if (!value) {
-      throw new Error(`${key} must be specified`);
+      logger.error(`${key} must be specified`)
+      hasError = true;
+    } else {
+      appConfig[key] = value;
     }
-
-    appConfig[key] = value;
   }
+  if (hasError) process.exit(1);
   logger.info('Loaded app config values');
 }
