fdb06331b12af87bf61214b1cca4fb2f475731ef
Junaid <junaidofficialnow@gmail.com>
2024-04-20 00:27:49 +0530

    feat: implemented pagination for user  listing  in admin panel

--- libs/users-client/notifications/src/lib/notifications/notifications.component.ts
+++ libs/users-client/notifications/src/lib/notifications/notifications.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { selectCurrentUserIdOrUsername } from '@food-stories/users-client/shared/app-init';
+import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'fs-notifications',
@@ -8,4 +11,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css'],
 })
-export class NotificationsComponent {}
+export class NotificationsComponent implements OnInit {
+  ngOnInit(): void {
+    this.store.select(selectCurrentUserIdOrUsername).subscribe((id) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      this.http.get<{notifications: any[]}>('http://localhost:3000/api/v1/users/notifications/' + id.id).subscribe((res) => {
+       this.notifications = res.notifications
+      })
+
+    })
+  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  notifications: any[] = []
+  store = inject(Store)
+  http = inject(HttpClient)
+
+
+
+}
